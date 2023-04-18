d3b4ca313bfca61417b5190cc6f300518d40e614
Alexander <125283933+Chixablox@users.noreply.github.com>
2023-04-18 09:28:17 +0600

    Update and rename Метод ветвей и границ(для правых).cs) to Метод ветвей и границ(для правых).cs

--- Метод ветвей и границ(для правых).cs
+++ Метод ветвей и границ(для правых).cs
@@ -11,61 +11,61 @@ namespace Метод_ветвей_и_границ
             Console.WriteLine("Введите число вершин");
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите число рёбер");
-            int m = Convert.ToInt32(Console.ReadLine());
-            int[,] smez = new int[n+1, n+1];
-            for(var i =0; i<m; i++)
+            int[,] smez = new int[n + 1, n + 1];
+            for (var i = 1; i < n + 1; i++)
             {
-                Console.WriteLine("Введите первую вершину");
-                int v1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите вторую вершину");
-                int v2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите вес ребра");
-                smez[v1, v2] = Convert.ToInt32(Console.ReadLine());
-            }
-            for(var i = 1; i<n+1; i++)
-            {
-                for(var j = 1; j<n+1; j++)
+                for (var j = 1; j < n + 1; j++)
                 {
-                    if (i == j)
+                    Console.Clear();
+                    for (var k = 1; k < n + 1; k++)
+                    {
+                        for (var l = 1; l < n + 1; l++)
+                        {
+                            Console.Write(smez[k,l] + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Введите вес ребра между вершинами " + i + " и " + j);
+                    int value = Convert.ToInt32(Console.ReadLine());
+                    smez[i, j] = value;
+                }
+            }
+            Console.Clear();
+            for (var i = 1; i < n + 1; i++)
+            {
+                for (var j = 1; j < n + 1; j++)
+                {
+                    if ((i == j) || (smez[i, j] == 0))
                     {
                         smez[i, j] = 9999;
                     }
                 }
             }
-            for(var j=0; j<n+1; j++)
+            for (var j = 0; j < n + 1; j++)
             {
                 smez[0, j] = j;
                 smez[j, 0] = j;
             }
-            for (var i = 0; i < n + 1; i++)
-            {
-                for (var j = 0; j < n + 1; j++)
-                {
-                    Console.Write(smez[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
             int min = 9999;
             int put = 0;
-            List<int> ver = new List<int>();
             //Алгоритм
-            for(var k = 0; k<n-1; k++)
+            for (var k = 0; k < n - 1; k++)
             {
                 //Минимумы в строках
-                for (var i =1; i<n+1; i++)
+                for (var i = 1; i < n + 1; i++)
                 {
                     if (smez[i, 0] == 10000)
                     {
                         continue;
                     }
-                    for (var j =1; j<n+1; j++)
+                    for (var j = 1; j < n + 1; j++)
                     {
-                        if(smez[i,j] < min)
+                        if (smez[i, j] < min)
                         {
                             min = smez[i, j];
                         }
                     }
-                    for(var j=1; j<n+1; j++)
+                    for (var j = 1; j < n + 1; j++)
                     {
                         if ((smez[i, j] != 9999) && (smez[i, j] != 10000))
                             smez[i, j] = smez[i, j] - min;
@@ -74,22 +74,22 @@ namespace Метод_ветвей_и_границ
                     min = 9999;
                 }
                 //Минимумы в столбцах
-                for(var j =1; j<n+1; j++)
+                for (var j = 1; j < n + 1; j++)
                 {
-                    if(smez[0,j] == 10000)
+                    if (smez[0, j] == 10000)
                     {
                         continue;
                     }
-                    for(var i = 1; i<n+1; i++)
+                    for (var i = 1; i < n + 1; i++)
                     {
-                        if(smez[i,j] < min)
+                        if (smez[i, j] < min)
                         {
                             min = smez[i, j];
                         }
                     }
                     for (var i = 1; i < n + 1; i++)
                     {
-                       if ((smez[i, j] != 9999) && (smez[i, j] != 10000))
+                        if ((smez[i, j] != 9999) && (smez[i, j] != 10000))
                             smez[i, j] = smez[i, j] - min;
                     }
                     put = put + min;
@@ -101,23 +101,23 @@ namespace Метод_ветвей_и_границ
                 int v1 = 0;
                 int v2 = 0;
                 //Выбираем степени нулей
-                for (var i =1; i<n+1; i++)
+                for (var i = 1; i < n + 1; i++)
                 {
-                    if(smez[i,0] == 10000)
+                    if (smez[i, 0] == 10000)
                     {
                         continue;
                     }
-                    for(var j=1; j<n+1; j++)
+                    for (var j = 1; j < n + 1; j++)
                     {
-                        if(smez[i,j] == 0)
+                        if (smez[i, j] == 0)
                         {
-                            for(var c =1; c<n+1; c++)
+                            for (var c = 1; c < n + 1; c++)
                             {
-                                if ((smez[i, c] < minstroka) && (c!=j))
+                                if ((smez[i, c] < minstroka) && (c != j))
                                 {
                                     minstroka = smez[i, c];
                                 }
-                                if((smez[c,j]< minstolb)&& (c != i))
+                                if ((smez[c, j] < minstolb) && (c != i))
                                 {
                                     minstolb = smez[c, j];
                                 }
@@ -134,23 +134,18 @@ namespace Метод_ветвей_и_границ
                     }
                 }
                 //Взаимодействие с этой максимальной степенью
-                ver.Add(v1);
-                ver.Add(v2);
-                if (smez[v2,v1] != 9999)
+                if (smez[v2, v1] != 9999)
                 {
                     smez[v2, v1] = 9999;
                 }
-                for(var i =0; i<n+1; i++)
+                for (var i = 0; i < n + 1; i++)
                 {
                     smez[v1, i] = 10000;
                     smez[i, v2] = 10000;
                 }
             }
-            Console.WriteLine(put);
-            /*for(var i =0; i<ver.Count; i++)
-            {
-                Console.Write(ver[i] + " ");
-            }*/
+            Console.WriteLine("Путь: " + put);
+            Console.ReadLine();
         }
     }
 }
